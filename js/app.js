9d7250e82cf4200d261c6f8fdeaf222145831b81
Alberto <74522184+albim360@users.noreply.github.com>
2023-02-23 15:02:52 +0100

    FEAT js per mostrare chat corrente con mex e data

--- js/app.js
+++ js/app.js
@@ -1,150 +1,169 @@
-const { createApp } = Vue
+const { createApp } = Vue;
 const app = createApp({
   data() {
     return {
-      activeIndex: null, 
+      sentMessage: null,
+      activeContact: {},
+      activeIndex: 0,
       contacts: [
         {
-          name: 'Psicologo',
-          avatar: './img/psicologo.jpg',
+          name: "Psicologo",
+          avatar: "./img/psicologo.jpg",
           visible: true,
           messages: [
             {
-              date: '22/02/2023 10:45:00',
-              message: 'Vorrei una seduta',
-              status: 'sent'
+              date: "22/02/2023 10:45:00",
+              message: "Vorrei una seduta",
+              status: "sent",
             },
             {
-              date: '20/02/2023 10:46:00',
-              message: 'Ripijatiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii',
-              status: 'received'
-            }
-          ]
+              date: "20/02/2023 10:46:00",
+              message: "Ripijatiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii",
+              status: "received",
+            },
+          ],
         },
         {
-          name: 'Psicologo',
-          avatar: './img/psicologo.jpg',
+          name: "Psicologo",
+          avatar: "./img/psicologo.jpg",
           visible: true,
           messages: [
             {
-              date: '22/02/2023 10:45:00',
-              message: 'Vorrei una seduta',
-              status: 'sent'
+              date: "22/02/2023 10:45:00",
+              message: "Vorrei una seduta",
+              status: "sent",
             },
             {
-              date: '20/02/2023 10:46:00',
-              message: 'Ripijatiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii',
-              status: 'received'
-            }
-          ]
+              date: "20/02/2023 10:46:00",
+              message: "Ripijatiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii",
+              status: "received",
+            },
+          ],
         },
         {
-          name: 'Psicologo',
-          avatar: './img/psicologo.jpg',
+          name: "Psicologo",
+          avatar: "./img/psicologo.jpg",
           visible: true,
           messages: [
             {
-              date: '22/02/2023 10:45:00',
-              message: 'Vorrei una seduta',
-              status: 'sent'
+              date: "22/02/2023 10:45:00",
+              message: "Vorrei una seduta",
+              status: "sent",
             },
             {
-              date: '20/02/2023 10:46:00',
-              message: 'Ripijatiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii',
-              status: 'received'
-            }
-          ]
+              date: "20/02/2023 10:46:00",
+              message: "Ripijatiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii",
+              status: "received",
+            },
+          ],
         },
         {
-          name: 'Psicologo',
-          avatar: './img/psicologo.jpg',
+          name: "Psicologo",
+          avatar: "./img/psicologo.jpg",
           visible: true,
           messages: [
             {
-              date: '22/02/2023 10:45:00',
-              message: 'Vorrei una seduta',
-              status: 'sent'
+              date: "22/02/2023 10:45:00",
+              message: "Vorrei una seduta",
+              status: "sent",
             },
             {
-              date: '20/02/2023 10:46:00',
-              message: 'Ripijatiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii',
-              status: 'received'
-            }
-          ]
+              date: "20/02/2023 10:46:00",
+              message: "Ripijatiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii",
+              status: "received",
+            },
+          ],
         },
         {
-          name: 'Psicologo',
-          avatar: './img/psicologo.jpg',
+          name: "Psicologo",
+          avatar: "./img/psicologo.jpg",
           visible: true,
           messages: [
             {
-              date: '22/02/2023 10:45:00',
-              message: 'Vorrei una seduta',
-              status: 'sent'
+              date: "22/02/2023 10:45:00",
+              message: "Vorrei una seduta..",
+              status: "sent",
             },
             {
-              date: '20/02/2023 10:46:00',
-              message: 'Ripijatiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii',
-              status: 'received'
-            }
-          ]
+              date: "20/02/2023 10:46:00",
+              message: "Ripijatiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii",
+              status: "received",
+            },
+          ],
         },
         {
-          name: 'Giulia',
-          avatar: './img/dogegg.jpg',
+          name: "Giulia",
+          avatar: "./img/dogegg.jpg",
           visible: true,
           messages: [
             {
-              date: '19/02/2023 15:00:00',
-              message: 'Ciao, mi puoi aiutare con il mio nuovo progetto?',
-              status: 'received'
+              date: "19/02/2023 15:00:00",
+              message: "Ciao, mi puoi aiutare con il mio nuovo progetto?",
+              status: "received",
             },
             {
-              date: '19/02/2023 16:00:00',
-              message: 'Certo, fammi sapere cosa ti serve',
-              status: 'sent'
+              date: "19/02/2023 16:00:00",
+              message: "Certo, fammi sapere cosa ti serve",
+              status: "sent",
             },
             {
-              date: '19/02/2023 16:30:00',
-              message: 'Grazie mille, sei sempre disponibile!',
-              status: 'received'
-            }
-          ]
+              date: "19/02/2023 16:30:00",
+              message: "Grazie mille, sei sempre disponibile!",
+              status: "received",
+            },
+          ],
         },
         {
-          name: 'Luca',
-          avatar: './img/wut.jpg',
+          name: "Luca",
+          avatar: "./img/wut.jpg",
           visible: true,
           messages: [
             {
-              date: '18/02/2023 10:00:00',
-              message: 'Ciao, come va?',
-              status: 'received'
+              date: "18/02/2023 10:00:00",
+              message: "Ciao, come va?",
+              status: "received",
             },
             {
-              date: '18/02/2023 10:30:00',
-              message: 'Sto bene, grazie! E tu?',
-              status: 'sent'
+              date: "18/02/2023 10:30:00",
+              message: "Sto bene, grazie! E tu?",
+              status: "sent",
             },
             {
-              date: '18/02/2023 11:00:00',
-              message: 'Anch\'io grazie, Che programmi hai per oggi?',
-              status: 'received'
+              date: "18/02/2023 11:00:00",
+              message: "Anch'io grazie, Che programmi hai per oggi?",
+              status: "received",
             },
             {
-              date: '18/02/2023 11:30:00',
-              message: 'Niente di particolare, forse andrò a fare una passeggiata, Tu?',
-              status: 'sent'
-            }
-          ]
-        }
+              date: "18/02/2023 11:30:00",
+              message:
+                "Niente di particolare, forse andrò a fare una passeggiata, Tu?",
+              status: "sent",
+            },
+          ],
+        },
       ],
     };
   },
+
   methods: {
     setActive(index) {
-      // aggiorna l'indice dell'elemento attivo
       this.activeIndex = index;
+      this.activeContact = this.contacts[index];
+      let messages = this.activeContact.messages;
+      this.sentMessage = messages[messages.length - 1];
     },
   },
-}).mount('#app');
+
+  directives: {
+    "message-class": {
+      mounted(el, binding) {
+        const status = binding.value;
+        el.classList.add(status === "sent" ? "send" : "receive");
+      },
+    },
+  },
+
+  mounted() {
+    this.setActive(this.activeIndex);
+  },
+}).mount("#app");
